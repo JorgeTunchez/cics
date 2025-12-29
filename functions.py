ee6b35f69c8acff498b418a6e628c9fcc454df99
Tunchez Perez <83575@bi.com.gt>
2025-12-29 17:07:52 -0600

    se realizo conexion a bd sql server; se valido la lectura de todos los archivos, se agregaron nuevos campos a base de datos

--- functions.py
+++ functions.py
@@ -1,5 +1,12 @@
+import json
 import re
+import sqlite3  
+import datetime
+import os
+from conexionBD import *
 
+
+# Funciones para procesar el archivo CICSADM.TXT
 def extraer_lineas(texto):
     lineas = []
 
@@ -13,7 +20,10 @@ def extraer_lineas(texto):
     return lineas
 
 
+# Extraer el título del segmento de una línea TS
 def extrearTitulo_segmento(linea):
+
+    arrTitulos = []
     # el titulo del segmento comienza despues del primer guion o numero
     match = re.search(r'[-0-9](.*)', linea) 
     if match:
@@ -21,12 +31,15 @@ def extrearTitulo_segmento(linea):
     else:
         titulo_segmento = linea.strip()
 
-    # Limpiar espacios adicionales
-    titulo_segmento = re.sub(r'\s+', ' ', titulo_segmento)
-    
-    return titulo_segmento
+
+    #dividir en dos partes si la separacion es muy grande y regresaar un array con las dos o mas partes
+    arrTitulos = re.split(r'\s{10,}', titulo_segmento)
+    #print(f"Titulo(s) segmento extraido: {arrTitulos}")
+
+    return arrTitulos
 
 
+# Extraer campos y valores de una línea DE
 def extraer_campos(texto):
     # Expresión regular más robusta:
     # Captura "campo" seguido de ":" y luego "valor"
@@ -50,6 +63,8 @@ def extraer_campos(texto):
             campos.append((campo, valor))
     return campos
 
+
+# Identificar el tipo de línea
 def identificar_tipo_linea(linea):
     if not linea:
         return 'VA'  #vacia  
@@ -68,3 +83,56 @@ def identificar_tipo_linea(linea):
         return 'FD'  #fin detalle
     else:
         return 'DE'  #detalle
+    
+# Validar si ya existe un segmento con la misma fecha
+def validarFecha(nombreArchivo, fecha_str):
+    #validar si en base de datos ya existe un segmento con la misma 
+    conn = conectar_base_datos()
+
+    cursor = conn.cursor()
+    cursor.execute("SELECT COUNT(*) FROM validacion_sistema WHERE archivo = ? AND fecha = ?", (nombreArchivo, fecha_str))   
+    count = cursor.fetchone()[0]
+    conn.close()    
+    return count
+
+
+# Insertar segmentos en la base de datos
+def insertarSegmentos(fechaActual, nombreArchivo, diccionarioSegmentos):
+
+    #crear conexion con base de datos SQL 
+    conn_sqlserver = conectar_base_datos()
+    cursor_sqlserver = conn_sqlserver.cursor()  
+    #crear tabla segmentos si no existe
+    cursor_sqlserver.execute('''IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='validacion_sistema' AND xtype='U')
+            CREATE TABLE validacion_sistema       
+            (id INT IDENTITY(1,1) PRIMARY KEY,
+             archivo NVARCHAR(255),
+             segmento NVARCHAR(255),
+             campo NVARCHAR(255),
+             valor NVARCHAR(MAX),
+             fecha DATE)''')    
+    conn_sqlserver.commit()
+
+    cantidadRegFechaActual = validarFecha(nombreArchivo, fechaActual)
+    print( f"Cantidad de registros para la fecha {fechaActual}: {cantidadRegFechaActual}" )
+
+    if cantidadRegFechaActual > 0:
+        print(f"Ya existen segmentos registrados para la fecha {fechaActual}. No se insertarán nuevos registros.")
+        conn_sqlserver.close()
+        return
+    else:   
+        print(f"Insertando nuevos segmentos para la fecha {fechaActual}.")      
+
+        for segmento_id, segmento in diccionarioSegmentos.items():
+            titulo = segmento['titulo']
+            detalles_json = json.dumps(segmento['detalles'])  
+
+            #recorrer los detalles
+            for detalles_json in segmento['detalles']:
+                #obtener campo y valor
+                for campo, valor in detalles_json.items():
+                    print(f"Archivo: {nombreArchivo}, Segmento: {titulo}, Campo: {campo}, Valor: {valor}")
+                    cursor_sqlserver.execute("INSERT INTO validacion_sistema (archivo, segmento, campo, valor, fecha) VALUES (?, ?, ?, ?, ?)", (nombreArchivo, titulo, campo, valor, fechaActual))
+        conn_sqlserver.commit()
+        conn_sqlserver.close()   
+        print("Inserción de validación completada.")
--- main.py
+++ main.py
@@ -4,83 +4,79 @@ import json
 import os
 from functions import *
 
-archivo_path = os.path.join('Reportes_CICS', 'CICSADM.TXT')
-# Leer el contenido del archivo solo las primera 150 lineas
-cantidadLeer = 150
-with open(archivo_path, 'r') as file:
-    texto = ''.join([next(file) for _ in range(cantidadLeer)])       
+# parametros
+# fechaActual = datetime.date.today().isoformat()
+fechaActual = '2025-12-30'
+cantidadLeer = 50
 
 
-lineas = extraer_lineas(texto)
-contador = 0
-contadorSegmentos = 0
-diccionarioSegmentos = {}
+#cantidadRegFechaActual = validarFechaSegmento(fechaActual)
+cantidadRegFechaActual = 0
+if cantidadRegFechaActual == 0:
 
-for linea in lineas:
-    contador += 1
-    tipoLinea = identificar_tipo_linea(linea)
+    # generar listado de archivos en carpeta Reportes_CICS
+    # archivos = os.listdir('Reportes_CICS')
+    archivos = os.listdir('Reportes_CICS_TEST')
 
-    if tipoLinea == 'IS':
-        contadorSegmentos += 1
+    for i in range(len(archivos)):
+        archivos[i] = archivos[i].upper()
+
+        print(f"Archivo en analisis: {archivos[i]}")
     
-    if tipoLinea == 'TS':
-        titulo_segmento = extrearTitulo_segmento(linea)
-        linea = titulo_segmento
-        diccionarioSegmentos[contadorSegmentos] = {'titulo': linea, 'detalles': []}
-
-    if tipoLinea == 'DE' and contadorSegmentos > 0:
-        campos = extraer_campos(linea)
-        for campo, valor in campos:
-            linea = f"{campo}: {valor}"
-            # al agrear los detalles, agregar como tupla campo: valor
-            diccionarioSegmentos[contadorSegmentos]['detalles'].append({campo: valor})
-
-    
-    if contador < 10:   
-        print(f"0{contador} | {tipoLinea} |{linea}")
-    else:   
-        print(f"{contador} | {tipoLinea} |{linea}")
-
-    
-
-print(f"\nTotal lineas procesadas: {contador}"
-      f"\nTotal segmentos encontrados: {contadorSegmentos}")
-
-#transformar diccionarioSegmentos a json
-
-json_data = json.dumps(diccionarioSegmentos, indent=4)  
-
-# Guardar el JSON en un archivo
-with open('segmentos.json', 'w') as json_file:
-    json_file.write(json_data)  
-
-# imprimir json_data
-print("\nJSON de segmentos y Campos:")
-print(json_data)
-
-# guardar el json en base de datos sqlite
-import sqlite3  
-conn = sqlite3.connect('segmentos.db')  
-c = conn.cursor()   
-# Crear tabla segmentos
-c.execute('''CREATE TABLE IF NOT EXISTS segmentos
-            (id INTEGER PRIMARY KEY AUTOINCREMENT,
-             segmento TEXT,
-             campo TEXT,
-             valor TEXT,
-             fecha DATE)''')     
-# Insertar datos en la tabla
-for segmento_id, segmento in diccionarioSegmentos.items():
-    titulo = segmento['titulo']
-    detalles_json = json.dumps(segmento['detalles'])  
-
-    #recorrer los detalles
-    for detalles_json in segmento['detalles']:
-        #obtener campo y valor
-        for campo, valor in detalles_json.items():
-            print(f"DB Segmento: {titulo}, Campo: {campo}, Valor: {valor}")
-            c.execute("INSERT INTO segmentos (segmento, campo, valor, fecha) VALUES (?, ?, ?, ?)", (titulo, campo, valor, datetime.date.today()))
-conn.commit()
-conn.close()    
+        archivo_path = os.path.join('Reportes_CICS', archivos[i])
+        # Leer el contenido del archivo solo las primera 150 lineas
+        with open(archivo_path, 'r') as file:
+            texto = ''.join([next(file) for _ in range(cantidadLeer)])       
 
 
+        lineas = extraer_lineas(texto)
+        contador = 0
+        contadorSegmentos = 0
+        diccionarioSegmentos = {}
+
+        for linea in lineas:
+            contador += 1
+            tipoLinea = identificar_tipo_linea(linea)
+
+            if tipoLinea == 'TS':
+                arrTitulos = extrearTitulo_segmento(linea)
+                for titulo in arrTitulos:
+                    contadorSegmentos += 1
+                    diccionarioSegmentos[contadorSegmentos] = {'titulo': titulo, 'detalles': []}
+
+
+            if tipoLinea == 'DE' and contadorSegmentos > 0:
+                campos = extraer_campos(linea)
+                for campo, valor in campos:
+                    linea = f"{campo}: {valor}"
+                    # al agregar los detalles, agregar como tupla campo: valor
+                    diccionarioSegmentos[contadorSegmentos]['detalles'].append({campo: valor})
+
+            
+            if contador < 10:   
+                print(f"0{contador} | {tipoLinea} |{linea}")
+            else:   
+                print(f"{contador} | {tipoLinea} |{linea}")
+
+            
+        print(f"\nTotal lineas procesadas: {contador}"
+            f"\nTotal segmentos encontrados: {contadorSegmentos}")
+
+        #transformar diccionarioSegmentos a json
+        json_data = json.dumps(diccionarioSegmentos, indent=4)  
+
+        # Guardar el JSON en un archivo
+        with open('segmentos.json', 'w') as json_file:
+            json_file.write(json_data)  
+
+        # imprimir json_data
+        print("\nJSON de segmentos y Campos:")
+        print(json_data)
+
+        # quitar extension al nombre del archivo
+        nombreArchivo = os.path.splitext(archivos[i])[0]  
+        print(f"\nNombre del archivo sin extension: {nombreArchivo}")  
+        insertarSegmentos(fechaActual, nombreArchivo, diccionarioSegmentos)
+
+else:
+    print(f"Ya existen {cantidadRegFechaActual} registros de segmentos para la fecha actual {fechaActual}. No se procesará el archivo nuevamente.")
